3ebee3f3c090c7ab59078fa7cbba289679f10d9b
z <62568509+zhuoming34@users.noreply.github.com>
2022-07-28 12:34:58 -0400

    Update enterpoint.sh

--- enterpoint.sh
+++ enterpoint.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 set -u
 
-echo "::set-output name=release-url::http://example.com"
+repo_token=$1
 
-exit 0
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+  echo "::set-output name=release-url::http://example.com"
+  exit 0
+fi
+
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
+
+if [ $event_type != "closed" ]; then
+  echo "::debug:::The event type was "$event_type 
+  exit 0
+fi
+
+milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
+
+# Internal Field Separator, read multiple variables
+IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
+
+release_url=$(dotnet gitreleasemanager create \ 
+--milestone $milestone_name \
+--targetcommitish $GITHUB_SHA \
+--token $repo_token \
+--owner $owner \
+--repository $repository)
+
+#echo "::set-output name=release-url::http://example.com"
+echo "::set-output name=release-url::$release_url"
+
+exit0
